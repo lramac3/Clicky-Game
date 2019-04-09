cf9bff8781f870a8eb06a7147fef7e2416cd1d60
Linu Ramachandran <linuram@gmail.com>
2019-04-09 17:04:12 -0400

    updated characters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 import Game from './components/Game'
 
 const App = () => (<Game />)
