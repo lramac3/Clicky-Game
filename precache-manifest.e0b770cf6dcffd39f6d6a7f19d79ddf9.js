ec525059b712bf197cd3cba3032e17d807049a45
Linu Ramachandran <linuram@gmail.com>
2019-04-12 13:43:40 -0400

    Updates

--- precache-manifest.e0b770cf6dcffd39f6d6a7f19d79ddf9.js
+++ precache-manifest.e0b770cf6dcffd39f6d6a7f19d79ddf9.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
   },
   {
-    "revision": "5e94e0154189c41144c7",
-    "url": "/Clicky-Game/static/js/main.bf4bce20.chunk.js"
+    "revision": "db1a53199ff2f6ce6c4d",
+    "url": "/Clicky-Game/static/js/main.416e9ad2.chunk.js"
   },
   {
     "revision": "3e33eb3cc5413fffd939",
     "url": "/Clicky-Game/static/js/2.6eb53f4d.chunk.js"
   },
   {
-    "revision": "5e94e0154189c41144c7",
+    "revision": "db1a53199ff2f6ce6c4d",
     "url": "/Clicky-Game/static/css/main.562446a4.chunk.css"
   },
   {
-    "revision": "cef7be3c81864325bdfbf65b51c71679",
+    "revision": "5a7bb5b766a38f281c8ea7652016a4d9",
     "url": "/Clicky-Game/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Clicky-Game/precache-manifest.72f8492ff22e55601e03720940d3cdcf.js"
+  "/Clicky-Game/precache-manifest.e0b770cf6dcffd39f6d6a7f19d79ddf9.js"
 );
 
 workbox.clientsClaim();
